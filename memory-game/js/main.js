0d4743371ed6410891b6d474f9a2908815917a83
Maria Dimitrijevic <marijaspirovska@yahoo.com>
2019-09-06 10:46:46 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,35 @@
-console.log("up and running!");
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardFour);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+
+alert('Hello, friends.');
+
+var cards = ["Queen", "Queen", "King", "King"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
+}
+
+
