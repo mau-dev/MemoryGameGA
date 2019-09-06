d81d69a7b9a5d5f6d6de0481d08d19c4d8a588af
Maria Dimitrijevic <marijaspirovska@yahoo.com>
2019-09-06 20:24:30 +0800

    created some logic with new functions and check for match

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,35 +1,26 @@
 
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardFour);
-
-
-
 alert('Hello, friends.');
 
 var cards = ["Queen", "Queen", "King", "King"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
 
-console.log("User flipped " + cardsInPlay[0]);
+    checkForMatch();
+};
 
-var cardTwo = cards[2];
+flipCard(0);
+flipCard(2);
 
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-
-	if (cardsInPlay[0] === cardsInPlay[2]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
-
-
+}
+console.log(cardsInPlay);
