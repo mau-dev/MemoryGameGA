465ffea6e9e9122b4c89d1823397c74b79981ae7
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-09-07 11:44:52 +0800

    created some logic, replaced array with objects

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,28 +1,57 @@
 
 alert('Hello, friends.');
 
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+   { 
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-hearts.png"
+  },
+   { 
+  	rank: "queen",
+  	suit: "diamonds",
+  	cardImage: "images/queen-of-diamonds.png"
+  },
+   { 
+  	rank: "king",
+  	suit: "hearts",
+  	cardImage: "images/king-of-hearts.png"
+  },
+  { 
+  	rank: "king",
+  	suit: "diamonds",
+  	cardImage: "images/king-of-diamonds.png"
+  }
+
+];
 
 var cardsInPlay = [];
 
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 
     checkForMatch();
+
+    
 };
 
 flipCard(0);
 flipCard(2);
 
-function checkForMatch() {
+ function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 }
 console.log(cardsInPlay);
 
-//check github username test
+
+
+
+
