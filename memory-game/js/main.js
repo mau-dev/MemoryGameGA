3ecd600b3a3a1d5fe0e4d48463248790de07bc03
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-09-07 12:23:25 +0800

    updated the checkForMatch function to make sure there are two cardsInPlay before checking for match and alert

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,38 +1,65 @@
 
 alert('Hello, friends.');
 
+// the cards in an array of objects
+
 var cards = [
    { 
+
   	rank: "queen",
   	suit: "hearts",
   	cardImage: "images/queen-of-hearts.png"
+
   },
    { 
+
   	rank: "queen",
   	suit: "diamonds",
   	cardImage: "images/queen-of-diamonds.png"
+
   },
    { 
+
   	rank: "king",
   	suit: "hearts",
   	cardImage: "images/king-of-hearts.png"
+
   },
   { 
+
   	rank: "king",
   	suit: "diamonds",
   	cardImage: "images/king-of-diamonds.png"
-  }
 
+  }
 ];
+
+// an array to store the cards in play
 
 var cardsInPlay = [];
 
 
+// function to check if the flipped cards match after ensurenig there are two cards flipped
+
+function checkForMatch() { 
+ 	if (cardsInPlay.length === 2) {
+ 		  if (cardsInPlay[0] === cardsInPlay[1]) {
+                alert("You found a match!");
+                } else {
+                alert("Sorry, try again.");
+				}
+            }
+ }
+
+
+// function to console what is flipper and to store the card rank in the cardsInPlay array
+
 var flipCard = function (cardId) {
 	console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+    
 
     checkForMatch();
 
@@ -42,13 +69,8 @@ var flipCard = function (cardId) {
 flipCard(0);
 flipCard(2);
 
- function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again.");
-}
-}
+
+
 console.log(cardsInPlay);
 
 
